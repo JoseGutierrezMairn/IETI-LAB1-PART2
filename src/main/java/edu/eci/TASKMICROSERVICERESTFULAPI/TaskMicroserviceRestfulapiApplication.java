17e90800e218bc9aa16ff64e918efdc10c50c9a7
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-08-22 11:02:16 -0500

    Files from step 1

---
+++ src/main/java/edu/eci/TASKMICROSERVICERESTFULAPI/TaskMicroserviceRestfulapiApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.TASKMICROSERVICERESTFULAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMicroserviceRestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMicroserviceRestfulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/TASKMICROSERVICERESTFULAPI/TaskMicroserviceRestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.TASKMICROSERVICERESTFULAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMicroserviceRestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
